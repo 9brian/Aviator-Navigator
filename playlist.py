6cd91da38bbc225a9558e7cae4714ae0ee6796c7
9brian <104478722+9brian@users.noreply.github.com>
2022-05-13 16:27:31 -0700

    Add files via upload

--- playlist.py
+++ playlist.py
@@ -10,20 +10,30 @@ bootstrap = Bootstrap5(app)
 # which URL is associated function
 @app.route('/', methods =["GET", "POST"])
 def home():
-    if request.method == "POST":
-       # getting input with name = fname in HTML form
-       city_name = request.form.get("cityname")
-       # getting input with name = lname in HTML form 
-       state_name = request.form.get("statename") 
-       return "The city you're looking at is " + city_name + " " + state_name
     return render_template("form.html")
+
+
 # Page 2
-@app.route('/how-it-works')
+@app.route('/how-it-works', methods = ["GET", "POST"])
 def page2func():
   return render_template('page2.html')
-@app.route('/experience')
+
+
+@app.route('/experience', methods = ["GET", "POST"])
 def page3func():
+  if request.method == "POST":
+    # getting input with name = fname in HTML form
+    city_name = request.form.get("cityname")
+    # getting input with name = lname in HTML form 
+    state_name = request.form.get("statename") 
+    return render_template('page4.html', cn=city_name, sn=state_name)
   return render_template('page3.html')
+
+
+@app.route('/e', methods = ["GET", "POST"])
+def page4func():
+   
+   return render_template('page4.html')
 
   
 if __name__=='__main__':
