7f1433f9dd02d6dbf3d09f53e05b56747b6b86d7
9brian <104478722+9brian@users.noreply.github.com>
2022-05-15 20:22:38 -0700

    Add files via upload

--- playlist.py
+++ playlist.py
@@ -1,17 +1,22 @@
 # importing Flask and other modules
 from flask import Flask, request, render_template 
 from flask_bootstrap import Bootstrap5 
+import requests, json
+from pprint import pprint
 
 # Flask constructor
 app = Flask(__name__)
-bootstrap = Bootstrap5(app)  
-  
+bootstrap = Bootstrap5(app) 
+
+# location = input("Where would you like to check the weather for? ")
+
+val_arr = []
+
 # A decorator used to tell the application
 # which URL is associated function
-@app.route('/', methods =["GET", "POST"])
+@app.route('/', methods =["GET", "POST"])  
 def home():
-    return render_template("form.html")
-
+  return render_template("form.html")
 
 # Page 2
 @app.route('/how-it-works', methods = ["GET", "POST"])
@@ -22,19 +27,51 @@ def page2func():
 @app.route('/experience', methods = ["GET", "POST"])
 def page3func():
   if request.method == "POST":
-    # getting input with name = fname in HTML form
     city_name = request.form.get("cityname")
-    # getting input with name = lname in HTML form 
     state_name = request.form.get("statename") 
     return render_template('page4.html', cn=city_name, sn=state_name)
   return render_template('page3.html')
 
-
 @app.route('/e', methods = ["GET", "POST"])
 def page4func():
-   
-   return render_template('page4.html')
-
+  location = 'marina'
+  url = f'https://weatherdbi.herokuapp.com/data/weather/{location}'
+  endpoint = url
   
+  payload = {
+    # 'region' : 'Marina, CA'
+  }
+  r = requests.get(endpoint, params=payload)
+  data = r.json()
+  # print(data)
+  region = data['region']
+  weatherNow = data['currentConditions']
+  
+  for key, value in weatherNow.items():
+    if(key != 'iconURL'):
+      if(key == 'temp'):    
+        val_arr.append(value)
+      elif(key == 'wind'):
+        val_arr.append(value)
+      else:
+        val_arr.append(value)
+   
+  dot = val_arr[0]
+  temp_cel = val_arr[1]['c']
+  temp_far = val_arr[1]['f']
+  precip = val_arr[2]
+  humid = val_arr[3]
+  wind_km = val_arr[4]['km']
+  wind_mile = val_arr[4]['mile']
+  comment = val_arr[5]
+
+
+
+  return render_template('page4.html', region=region,
+  dot=dot, cel=temp_cel, far=temp_far, precip=precip, 
+  humid=humid, wind_km=wind_km, wind_mile=wind_mile,
+  comment=comment)
+
+    
 if __name__=='__main__':
-   app.run()
+  app.run()
